84fcafe144761bde30170633ecc38e83813a4a42
17aclemons <17aclemons@gmail.com>
2019-11-18 10:59:17 -0700

    Jaegar fixed the order of the code

--- Final Project.sql
+++ Final Project.sql
@@ -1,14 +1,38 @@
-#Jaegars Code
+create table TEAMS(
+TeamID int primary key, 
+TeamName varchar(255), 
+MatchesWon int, 
+MatchesLost int);
+
 #Players Table
 create table PLAYERS (PlayerID int(255) primary key, FirstName varchar(255), 
-LastName varchar(255), Age int(255), CurrentTeam varchar(255));
+LastName varchar(255), Age int(255), CurrentTeam int(255));
 alter table PLAYERS add constraint ct_fk foreign key PLAYERS (CurrentTeam) references TEAMS (TeamID);
 
-/*
-Matches Played (Player ID, Match ID)
-Previous Teams (Player ID, Team ID)
-Match History ( Match ID, Team ID, score)
-*/
+#Coaches
+create table COACHES (CoachID int , Name varchar(4), age int, CurrentTeam varchar(255), TeamID int);
+
+
+
+alter table COACHES add constraint coach_pk primary key COACHES (CoachID, TeamID);
+
+#Matches
+create table MATCHES (MatchID int, Field varchar(255), Time varchar (5), GameDate Date , Match_Type Enum ('Future', 'Played'), FieldID int);
+alter table MATCHES add constraint ma_pk primary key (MatchID, FieldID);
+
+#Future Match
+Create table FUTURE_MATCH (MatchID int(255), Time varchar (255));
+Alter table FUTURE_MATCH add constraint fm_pk primary key(MatchID);
+Alter table FUTURE_MATCH add constraint fm_fk foreign key FUTURE_MATCH (MatchID) references MATCHES (MatchID);
+
+#Played Match
+Create table PLAYED_MATCH (MatchID int primary key, Time varchar(5));
+Alter table PLAYED_MATCH add constraint pm_fk foreign key PLAYED_MATCH (MatchID) references MATCHES (MatchID);
+
+#Fields
+create table FIELDS (FieldID int primary key, FieldName varchar(255), 
+Street varchar(255), City varchar(255), State varchar(255), Zip int, StadiumCap int);
+
 create table PREVIOUS_TEAMS (PlayerID int(255), TeamID int(255));
 alter table PREVIOUS_TEAMS add constraint pt_pk primary key (PlayerID, TeamID);
 alter table PREVIOUS_TEAMS add constraint pt_fk1 foreign key PREVIOUS_TEAMS (PlayerID) references PLAYERS (PlayerID);
@@ -17,28 +41,26 @@ alter table PREVIOUS_TEAMS add constraint pt_fk2 foreign key PREVIOUS_TEAMS (Tea
 create table MATCHES_PLAYED (PlayerID int(255), MatchID int(255));
 alter table MATCHES_PLAYED add constraint mp_pk primary key (PlayerID, MatchID);
 alter table MATCHES_PLAYED add constraint mp_fk1 foreign key MATHCES_PLAYED (PlayerID) references PLAYERS (PlayerID);
-alter table MATCHES_PLAYED add constraint mp_fk2 foreign key MATCHES_PLAYED (MatchID) references MATCHES (MatchID);
+
+
+
+alter table MATCHES_PLAYED add constraint mp2_fk2 foreign key MATCHES_PLAYED (MatchID) references MATCHES (MatchID);
 
 create table MATCH_HISTORY (MatchID int(255), TeamID int(255));
 alter table MATCH_HISTORY add constraint mh_pk primary key (MatchID, TeamID);
 alter table MATCH_HISTORY add constraint mh_fk1 foreign key MATCH_HISTORY (MatchID) references MATCHES (MatchID);
 alter table MATCH_HISTORY add constraint mh_fk2 foreign key MATCH_HISTORY (TeamID) references TEAMS (TeamID);
 
-#Andrew's Code
-create table TEAMS(
-TeamID int primary key, 
-TeamName varchar(255), 
-MatchesWon int, 
-MatchesLost int);
+
 
 create table CURRENT_TEAM_MEMBERS(
 TeamID int,
-PlayerID int,
+PlayerID int, 
 primary key (TeamID, PlayerID), 
 constraint fk_teamid foreign key (TeamID)
-references Teams(TeamID),
+references TEAMS(TeamID),
 constraint fk_playerid foreign key (PlayerID)
-references PLAYER(PlayerID)); 
+references PLAYERS(PlayerID)); 
 
 create table CURRENT_COACHES(
 TeamID int,
@@ -55,23 +77,14 @@ CoachID int,
 TeamID int,
 primary key (CoachID, TeamID),
 constraint fk_ptcCoachID foreign key (CoachID)
-references Coaches(CoachID),
+references COACHES(CoachID),
 constraint fk_ptcTeamID foreign key (TeamID)
 references TEAMS(TeamID)
 );
 
-#Fields
-create table FIELDS (FieldID int primary key, FieldName varchar(255), 
-Street varchar(255), City varchar(255), State varchar(255), Zip int, StadiumCap int);
-
-#Matches
-create table MATCHES (MatchID int, WinningTeam varchar(4), Score int, Field varchar(255), TimePlayed varchar(255), GameDate Date, MatchCompleted varchar(3), FieldID int primary key);
-
 #Teams Playing
 create table TEAMS_PLAYING (MatchID int(255), TeamID int(255));
 alter table TEAMS_PLAYING add constraint mp_pk primary key (MatchID, TeamID);
-alter table TEAMS_PLAYING add constraint mp_fk1 foreign key TEAMS_PLAYING (MatchID) references MATCHES (MatchID);
-alter table TEAMS_PLAYING add constraint mp_fk2 foreign key TEAMS_PLAYING (TeamID) references TEAMS (TeamID);
 
-#Coaches
-create table COACHES (CoachID int primary key , Name varchar(4), age int, CurrentTeam varchar(255), TeamID int primary key);
+alter table TEAMS_PLAYING add constraint tp_fk1 foreign key TEAMS_PLAYING (MatchID) references MATCHES (MatchID);
+alter table TEAMS_PLAYING add constraint tp_fk2 foreign key TEAMS_PLAYING (TeamID) references TEAMS (TeamID);
